a0d30cec984b79e9da97a6abddd325d93b65f4c9
Tim 'mithro' Ansell <me@mith.ro>
2021-11-27 13:29:57 -0800

    Also get the IoT sheet.

--- dnsmasq.py
+++ dnsmasq.py
@@ -1,39 +1,57 @@
 #!/usr/bin/env python3
 
 import csv
+import json
 import pprint
 import re
+import sys
 import urllib.request
 
-GDOC="https://docs.google.com/spreadsheets/d/e/2PACX-1vR5j6yiZCEv5YNoeVNLM4MMsxzBVjG4OtViBz7tXXF1LydHd8bCOOVWt7MvfVEPZtK0TeWgyxF3i9Tj/pub?output=csv"
-r = urllib.request.urlopen(GDOC)
-d = r.read().decode('utf-8').splitlines()
+GDOC={
+    'Network': "https://docs.google.com/spreadsheets/d/e/2PACX-1vR5j6yiZCEv5YNoeVNLM4MMsxzBVjG4OtViBz7tXXF1LydHd8bCOOVWt7MvfVEPZtK0TeWgyxF3i9Tj/pub?gid=1476589425&single=true&output=csv",
+    'IoT':     "https://docs.google.com/spreadsheets/d/e/2PACX-1vR5j6yiZCEv5YNoeVNLM4MMsxzBVjG4OtViBz7tXXF1LydHd8bCOOVWt7MvfVEPZtK0TeWgyxF3i9Tj/pub?gid=1695016218&single=true&output=csv",
+}
+#    "https://docs.google.com/spreadsheets/d/e/2PACX-1vR5j6yiZCEv5YNoeVNLM4MMsxzBVjG4OtViBz7tXXF1LydHd8bCOOVWt7MvfVEPZtK0TeWgyxF3i9Tj/pub?output=csv",
+# https://docs.google.com/spreadsheets/d/e/2PACX-1vR5j6yiZCEv5YNoeVNLM4MMsxzBVjG4OtViBz7tXXF1LydHd8bCOOVWt7MvfVEPZtK0TeWgyxF3i9Tj/pub?output=csv
+# https://docs.google.com/spreadsheets/d/e/2PACX-1vR5j6yiZCEv5YNoeVNLM4MMsxzBVjG4OtViBz7tXXF1LydHd8bCOOVWt7MvfVEPZtK0TeWgyxF3i9Tj/pub?gid=1476589425&single=true&output=csv
+# https://docs.google.com/spreadsheets/d/e/2PACX-1vR5j6yiZCEv5YNoeVNLM4MMsxzBVjG4OtViBz7tXXF1LydHd8bCOOVWt7MvfVEPZtK0TeWgyxF3i9Tj/pub?gid=1695016218&single=true&output=csv
+
+data = []
+for name, doc in GDOC.items():
+    r = urllib.request.urlopen(doc)
+    csv_data = r.read().decode('utf-8').splitlines()
+
+    for i, r in enumerate(csv.DictReader(csv_data)):
+        d = {}
+        for k, v in list(r.items()):
+            if v.strip():
+                d[k] = v
+
+        if '' in d:
+            del d['']
+
+        if not d:
+            continue
+        data.append(((name, i), d))
+
+with open('info.json', 'w') as f:
+    json.dump(data, f, indent="  ", sort_keys=True)
 
 OUTPUT = 'dnsmasq.static.conf'
 output = open(OUTPUT, 'w')
-for i, r in enumerate(csv.DictReader(d)):
-    d = {}
-    for k, v in list(r.items()):
-        if v.strip():
-            d[k] = v
-
-    if not d:
-        continue
-
-    del r['']
-
+for (name, lineno), r in data:
     skip = []
-    if not r['MAC Address']:
+    if not r.get('MAC Address', None):
         skip.append('No MAC address')
 
-    if not r['Machine']:
+    if not r.get('Machine', None):
         skip.append('No machine name')
 
-    if not r['IP']:
+    if not r.get('IP', None):
         skip.append('No IP address')
 
     if skip:
-        print('Skipping row', i+1, ':', ', '.join(skip))
+        print('Skipping row', name, lineno+1, ':', ', '.join(skip))
         pprint.pprint(r)
         print()
         continue
@@ -47,7 +65,7 @@ for i, r in enumerate(csv.DictReader(d)):
         print('# {}: {}'.format(k, v), file=output)
 
     dhcp_name = r['Machine'].lower().strip()
-    if r['Interface'].strip():
+    if 'Interface' in r and r['Interface'].strip():
         dhcp_name = r['Interface'].lower().strip()+'.'+dhcp_name
 
     simple_dhcp_name = re.sub('[^a-z0-9.\-_]+','#',dhcp_name)
